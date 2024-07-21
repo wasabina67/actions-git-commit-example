e49be3bbef279cbf642191dd203072bce954b409
wasabina67 <wasabina67@gmail.com>
2024-07-21 17:13:23 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,10 @@ else
     echo ".env is not found."
     exit 1
 fi
+
+curl \
+    -X POST \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token $PAT" \
+    https://api.github.com/repos/wasabina67/actions-git-commit-example/dispatches \
+    -d '{"event_type": "git-commit"}'
